66515278c675b502f69cb1d29b66965e3915e1ca
Mehdi Yari <mehdi.email.info@gmail.com>
2020-05-04 01:48:55 +0430

    add `mock` flavor for holding test utilities for `AspectRatioImageViewTest`

---
+++ ariv/src/mock/kotlin/ir/mehdiyari/ariv/MockActivity.kt
@@ -0,0 +1,13 @@
+package ir.mehdiyari.ariv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aspect_ratio_image_view_mock_view)
+    }
+
+}
